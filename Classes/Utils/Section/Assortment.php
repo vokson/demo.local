0b340c7c9bbde92f101ba43bfb1ac0db7d26686f
vokson@yandex.ru <vokson@yandex.ru>
2015-11-22 21:19:20 +0300

    0.1.02 - Change structure of assortment's classes

---
+++ Classes/Utils/Section/Assortment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Classes\Utils\Section;
+
+/**
+ * Class make string naem of section type
+ *
+ * @author Noskov Alexey
+ */
+ class Assortment {
+     
+     /*
+      * Make name of steel profile section
+      * 
+      * @param string var Variable for name
+      * @param string name Name of section
+      * 
+      * @return bool Success or NOT
+      */
+     function steelProfile (&$var, $name) {
+//         echo "NAME = ".$name."<br/>";
+         
+         if (isset($this->collection[$name])) {
+             $var = $this->collection[$name];
+             return TRUE;
+         } else {
+             $var = NULL;
+             return FALSE;
+         }
+     }
+}
